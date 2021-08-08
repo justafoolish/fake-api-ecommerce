a62764b8f19315d033347d801ea54c327d57a176
Hắc Tún <hactuns@MacBook-Pro-cua-Hac.local>
2021-08-08 16:29:07 +0700

    fix port

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
 
 // Use default router
-server.use(router)
-server.listen(8080, () => {
-  console.log('JSON Server is running')
-})
+const PORT = process.env.PORT || 80;
+
+server.use(router);
+server.listen(PORT, () => {
+  console.log("JSON Server is running");
+});
